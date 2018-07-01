5148f0e339bca0f49f0765d3bd233b153ea6f2fc
Your Name <you@example.com>
2018-07-01 16:20:39 +0530

    doe with the snake and ladder implementation still have to do the finishing moments and some improvements

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -1,6 +1,10 @@
 
 import UIKit
 
+struct FromTo {
+    var from: Int
+    var to: Int
+}
 enum Dice: Int {
     case one = 1, two, three, four, five, six, none = 0
 }
@@ -28,6 +32,8 @@ class ViewController: UIViewController {
     let playerOne: Player = Player(playerInfo: "Player one's turn", playerColor: #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1))
     let playerTwo: Player = Player(playerInfo: "Player two's turn", playerColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1))
     var dice: Dice = .none
+    let ladderCells: [(from: Int, to: Int)] = [(4, 20), (12, 26), (29, 48), (41, 65), (52, 73), (57, 82), (70, 85), (90, 100), (92, 95)]
+    let snakeCells: [(from: Int, to: Int)] = [(98, 23), (94, 67), (88, 46), (86, 53), (81, 44), (79, 61), (77, 55), (76, 13), (60, 15), (54, 1), (51, 30), (39, 5), (21, 17)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,24 +44,18 @@ class ViewController: UIViewController {
         showStartAlert()
         gameStarted()
     }
+    
+    // ladder cells: 4-20, 12-26, 29-48, 41-65, 52-73, 57-82, 70-85, 90-100, 92-95
+    // snake cells: 98-23, 94-67, 88-46, 86-53, 81-44, 79-61, 77-55, 76-13, 60-15, 54-1, 51-30, 39-5, 21-17
+    
     @IBAction func dicePressed(_ sender: UIButton) {
         let diceValue = rollDice()
         dice = Dice.init(rawValue: diceValue) ?? .none
         changeDiceImage()
         if player == .playerOne {
-            playerOne.playerPosition += diceValue
-            let boardPosition = view.viewWithTag(playerOne.playerPosition)
-            boardPosition?.addSubview(playerOne.playerView)
-            playerOneScoreLabel.text = "\(playerOne.playerPosition)"
-            playerInfoLabel.text = playerOne.playerInfo
-            player.changePlayer()
+            configurePlayerOne(diceValue)
         } else {
-            playerTwo.playerPosition += diceValue
-            let boardPosition = view.viewWithTag(playerTwo.playerPosition)
-            boardPosition?.addSubview(playerTwo.playerView)
-            playerTwoScoreLabel.text = "\(playerTwo.playerPosition)"
-            playerInfoLabel.text = playerTwo.playerInfo
-            player.changePlayer()
+            configurePlayerTwo(diceValue)
         }
     }
 }
@@ -89,6 +89,69 @@ extension ViewController {
             break
         }
     }
+    
+    func alertFunction(from: Int, to: Int, isLadder: Bool) {
+        var title = "";var message = ""
+        if isLadder {
+            title = "Luck favors you"
+            message = "You have climbed \(to - from) steps"
+        } else {
+            title = "Oops!"
+            message = "You have climbed down by \(from - to) steps"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // this function is to configure the player one values
+    fileprivate func configurePlayerOne(_ diceValue: Int) {
+        playerOne.playerPosition += diceValue
+        for ladderCell in ladderCells {
+            if ladderCell.from == playerOne.playerPosition {
+                // provide alert function
+                alertFunction(from: ladderCell.from, to: ladderCell.to, isLadder: true)
+                playerOne.playerPosition = ladderCell.to
+            }
+        }
+        for snakeCell in snakeCells {
+            if snakeCell.from == playerOne.playerPosition {
+                // provide alert function
+                alertFunction(from: snakeCell.from, to: snakeCell.to, isLadder: false)
+                playerOne.playerPosition = snakeCell.to
+            }
+        }
+        let boardPosition = view.viewWithTag(playerOne.playerPosition)
+        boardPosition?.addSubview(playerOne.playerView)
+        playerOneScoreLabel.text = "\(playerOne.playerPosition)"
+        playerInfoLabel.text = playerOne.playerInfo
+        player.changePlayer()
+    }
+    
+    // this function of to configure the player two values
+    fileprivate func configurePlayerTwo(_ diceValue: Int) {
+        playerTwo.playerPosition += diceValue
+        for ladderCell in ladderCells {
+            if ladderCell.from == playerTwo.playerPosition {
+                // provide alert function
+                alertFunction(from: ladderCell.from, to: ladderCell.to, isLadder: true)
+                playerTwo.playerPosition = ladderCell.to
+            }
+        }
+        for snakeCell in snakeCells {
+            if snakeCell.from == playerTwo.playerPosition {
+                // provide alert function
+                alertFunction(from: snakeCell.from, to: snakeCell.to, isLadder: false)
+                playerTwo.playerPosition = snakeCell.to
+            }
+        }
+        let boardPosition = view.viewWithTag(playerTwo.playerPosition)
+        boardPosition?.addSubview(playerTwo.playerView)
+        playerTwoScoreLabel.text = "\(playerTwo.playerPosition)"
+        playerInfoLabel.text = playerTwo.playerInfo
+        player.changePlayer()
+    }
+
 }
 
 class Player{
